8bf50762e90359f038f3dd121cb54afd5fd9d7cf
George <84743235+nikibeep@users.noreply.github.com>
2023-04-01 00:05:58 +0530

    first commit

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,20 @@
+
+ 
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>I</b>CI</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><img src="<?php echo base_url('assets/dist/img/JDLogoFin.png')?>" alt="Logo" width="30px" height="30px"></img> Jewellery Djinn </span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+    
+    </nav>
+  </header>
